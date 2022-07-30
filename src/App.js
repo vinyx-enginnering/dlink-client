6416efa0e8a7cf657d0114c34b10f491f12fb1c8
elechigeorge <elechigeorgepro@gmail.com>
2022-07-30 08:02:54 +0100

    Modified stuff

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navigation from "./components/Navigation.js";
 import Dashboard from "./screens/Dashboard.js";
